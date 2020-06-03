cd8041be0a7af05f48e26e9c7562cdc122c21de2
Bram <bram_vos@hotmail.com>
2020-06-03 09:45:14 +0200

    Added feedback & iteration

---
+++ Badge Animatie/script.js
@@ -0,0 +1,72 @@
+var circle = bodymovin.loadAnimation({
+    container: document.getElementById('circle'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'circle.json'
+})
+
+var circleLine = bodymovin.loadAnimation({
+    container: document.getElementById('circle-line'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'circle-line.json'
+})
+
+var targetIcon = bodymovin.loadAnimation({
+    container: document.getElementById('target-icon'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'target-icon.json'
+})
+
+var longestRunBadge = bodymovin.loadAnimation({
+    container: document.getElementById('longest-run-badge'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'longest-run-badge.json'
+})
+
+var targetBadge = bodymovin.loadAnimation({
+    container: document.getElementById('target-badge'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'target-badge.json'
+})
+
+var fastestKmBadge = bodymovin.loadAnimation({
+    container: document.getElementById('fastest-km-badge'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'fastest-km-badge.json'
+})
+
+var targetBadge2 = bodymovin.loadAnimation({
+    container: document.getElementById('target-badge-2'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'target-badge-2.json'
+})
+
+var fastestKmBadge2 = bodymovin.loadAnimation({
+    container: document.getElementById('fastest-km-badge-2'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'fastest-km-badge-2.json'
+})
+
+
+var longestRunBadge2 = bodymovin.loadAnimation({
+    container: document.getElementById('longest-run-badge-2'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'longest-run-badge-2.json'
+})
